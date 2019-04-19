a6e569c6963ce20f2eda92c380887c32c9d40e70
TechnicalServiceABRD@gmail.com <TechnicalServiceABRD@gmail.com>
2019-04-19 00:17:59 -0400

    Delete

--- reactSqlite/src/screens/Home.js
+++ reactSqlite/src/screens/Home.js
@@ -3,8 +3,10 @@ import { Text, View, ScrollView, Alert, Linking } from 'react-native';
 import { Header, Input, SearchBar, Button } from 'react-native-elements';
 import { Collapse, CollapseHeader, CollapseBody } from "accordion-collapse-react-native";
 import { Spinner, ListItem, Separator } from 'native-base';
-
-import { Font,SQLite } from 'expo';
+import IconF from 'react-native-vector-icons/FontAwesome';
+const AlertMsg = 'Estas seguro que deseas eliminar la informacion';
+const AlertCancel = 'Ha cancelado la eliminacion';
+import { Font, SQLite } from 'expo';
 
 const db = SQLite.openDatabase('Milagro.db')
 
@@ -14,17 +16,17 @@ export default class Home extends React.Component {
 
     async componentWillMount() {
         await Font.loadAsync({
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-          Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
         });
         db.transaction(tx => {
-          tx.executeSql(
-            'create table if not exists tbl_milagro (id integer primary key not null, name text,age text);'
-          );
-        }); 
+            tx.executeSql(
+                'create table if not exists tbl_milagro (id integer primary key not null, name text,age text);'
+            );
+        });
         this.setState({ loading: false });
-      }
+    }
     async componentDidMount() {
         const { search } = this.state;
         await this.fetchData(search);
@@ -52,6 +54,25 @@ export default class Home extends React.Component {
         });
     }
 
+    deleteData(id) {
+        var query = "DELETE FROM tbl_milagro WHERE id = ?";
+        var params = [id];
+        db.transaction((tx) => {
+            tx.executeSql(query, params, (tx, results) => {
+                Alert.alert("Success", "Ha sido eliminado correctamente");
+
+            }, function (tx, err) {
+                Alert.alert("Warning", "No se ha eliminado" + err);
+            });
+        });
+    }
+
+
+    async handleDelete(id) {
+        const { search } = this.state;
+        await this.deleteData(id);
+        this.fetchData(search);
+    }
 
     handleAdd() {
         this.props.navigation.navigate('Add');
@@ -63,11 +84,11 @@ export default class Home extends React.Component {
     render() {
         if (this.state.loading) {
             return (
-              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Spinner color="green" />
-              </View>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Spinner color="green" />
+                </View>
             );
-          }
+        }
         const listItem = this.state.dataTodo.map((item) =>
             <View key={item.id}>
                 <Collapse style={{ marginBottom: 10, marginTop: 10 }}>
@@ -75,15 +96,35 @@ export default class Home extends React.Component {
                         <Separator bordered>
                             <View style={{ flexDirection: 'row' }}>
                                 <Text style={{ fontSize: 14, color: '#000000' }}>{item.name}</Text>
+                                <Button
+                                    onPress={() => this.handleEdit(item.id)}
+                                    icon={<IconF name='eye' color='#2B2F33' size={21} />}
+                                    iconContainerStyle={{ marginRight: 20 }}
+                                    buttonStyle={{ height: 20, width: 80, backgroundColor: '#F0EEF4', marginLeft: 100 }}
+                                    titleStyle={{ fontSize: 11, color: '#000000' }}
+                                    title='  Actualizar'
+                                />
+                                <Button
+                                    onPress={() =>
+                                        Alert.alert('AVISO', AlertMsg, [
+                                            { text: 'Cancel', onPress: () => Alert.alert(AlertCancel) },
+                                            { text: 'ok', onPress: () => this.handleDelete(item.id) },
+
+                                        ])
+                                    }
+                                    icon={<IconF name='trash' color='#2B2F33' size={21} />}
+                                    buttonStyle={{ height: 20, width: 80, backgroundColor: '#F0EEF4', marginLeft: 15 }}
+                                    titleStyle={{ fontSize: 12, color: '#000000' }}
+                                    title='  Borrar' />
                             </View>
                         </Separator>
                     </CollapseHeader>
                     <CollapseBody>
                         <ListItem style={{ marginBottom: 3, marginTop: 3 }}>
-                            <Text><Text> Name: </Text>{item.name}</Text>
+                            <Text><Text> Name : </Text>{item.name}</Text>
                         </ListItem>
                         <ListItem style={{ marginBottom: 3, marginTop: 3 }}>
-                            <Text><Text> Age: </Text>{item.age}</Text>
+                            <Text><Text> Age : </Text>{item.age}</Text>
                         </ListItem>
                     </CollapseBody>
                 </Collapse>
@@ -111,9 +152,9 @@ export default class Home extends React.Component {
                 />
                 <ScrollView>
 
-                <View>
-                    {listItem}
-                </View>
+                    <View>
+                        {listItem}
+                    </View>
                 </ScrollView>
 
             </View>
